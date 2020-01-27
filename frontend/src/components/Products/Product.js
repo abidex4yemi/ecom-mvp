e6cfb15dbd959a64195af0502f66efb4f997651c
abidex4yemi <abidex4yemi@gmail.com>
2020-01-27 16:06:35 +0100

    Styled single product component

--- frontend/src/components/Products/Product.js
+++ frontend/src/components/Products/Product.js
@@ -1,31 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Product = props => {
-  const { name, description, attributes, _id } = props.productDetails;
+  const { name, attributes, _id } = props.productDetails;
   const { imageUrl } = attributes[0];
 
   return (
-    <article>
+    <StyledSingleProduct>
       <Link to={`/products/${_id}`}>
-        <img src={imageUrl} />
+        <img src={imageUrl} alt={name} />
       </Link>
 
-      <div>
-        <div>
-          <h3>{name}</h3>
-        </div>
-        <div>
-          <p>{description}</p>
-        </div>
-        <div>
-          <Link to={`/products/${_id}`}>
+      <StyledProductInfo>
+        <h3>{name}</h3>
+        <StyleButtonContainer>
+          <StyledShopNowButton to={`/products/${_id}`}>
             <span>Shop Now</span>
-          </Link>
-        </div>
-      </div>
-    </article>
+          </StyledShopNowButton>
+        </StyleButtonContainer>
+      </StyledProductInfo>
+    </StyledSingleProduct>
   );
 };
 
@@ -37,3 +33,49 @@ Product.propTypes = {
 };
 
 export default Product;
+
+const StyledSingleProduct = styled.article`
+  width: calc(100% / 4 + 0px);
+  margin-bottom: 40px;
+
+  img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
+const StyledProductInfo = styled.div`
+  padding: 16px 32px 0 32px;
+
+  h3 {
+    font-size: 1.2rem;
+    font-weight: bold;
+    line-height: 36px;
+    letter-spacing: 1;
+  }
+`;
+
+const StyleButtonContainer = styled.div`
+  text-align: center;
+`;
+
+const StyledShopNowButton = styled(Link)`
+  color: rgb(0, 0, 0);
+  font-weight: bold;
+  border: 2px solid rgb(0, 0, 0);
+  padding: 10px 30px;
+  text-align: center;
+  background: rgb(255, 255, 255);
+  display: inline-block;
+  overflow: hidden;
+  margin: 6px auto;
+  max-height: 44px;
+  max-width: 510px;
+  width: auto;
+  transition: 0.3s;
+
+  :hover {
+    background: rgb(0, 0, 0);
+    color: #fff;
+  }
+`;
