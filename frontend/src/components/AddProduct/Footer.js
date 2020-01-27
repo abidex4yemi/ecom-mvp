a1752b7064699d942d135d916e7f8ae2ddccc156
abidex4yemi <abidex4yemi@gmail.com>
2020-01-27 17:30:17 +0100

    Style add product footer component

--- frontend/src/components/AddProduct/Footer.js
+++ frontend/src/components/AddProduct/Footer.js
@@ -3,15 +3,43 @@ import styled from 'styled-components';
 
 const Footer = () => {
   return (
-    <footer>
-      <div>
-        <button>Cancel</button>
-        <button>Delete</button>
-        <button>Save</button>
-        <button>Save & Publish</button>
-      </div>
-    </footer>
+    <StyledFooter>
+      <button>Cancel</button>
+      <button>Delete</button>
+      <button>Save</button>
+      <button>Save & Publish</button>
+    </StyledFooter>
   );
 };
 
 export default Footer;
+
+const StyledFooter = styled.header`
+    display: flex;
+    justify-content: space-between;
+    border-top: 1px solid #dedede;
+    height: 40px;
+    
+
+    button {
+      display: block;
+      font-size: 0.8rem;
+      width: 100%;
+      border: none;
+      background: #f2f2f2;
+      border-right:1px solid #dedede;
+      cursor: pointer;
+      text-transform: uppercase;
+      outline: none;
+
+      :hover {
+        background: #8b8b8b;
+        color: #fff;
+      }
+
+      :last-child {
+      border-right: 0;
+    }
+    }
+  }
+`;
